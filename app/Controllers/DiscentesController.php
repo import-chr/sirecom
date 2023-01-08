7002ec3eb17669ea8540071391d370d036a10657
chirs <yosoypro_117@hotmail.com>
2023-01-08 12:42:01 -0600

    discentes controller

---
+++ app/Controllers/DiscentesController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Interface\Controller;
+use Database\PDO\Connection;
+use PDO;
+
+class DiscentesController implements Controller {
+
+    private $connection;
+
+    public function __construct() {
+        $this->connection = Connection::getInstance()->getDatabase();
+    }
+
+    public function index() {
+        $query = $this->connection->prepare("SELECT * FROM discentes");
+        $query->execute();
+
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);
+        // var_dump($results);
+
+        require("../source/views/discentes/index.php");
+    }
+
+    public function create() {
+        require("../source/views/discentes/create.php");
+    }
+
+    public function store($data) {
+        $query = $this->connection->prepare("INSERT INTO discentes (matricula, nombre, apellido) VALUES (:matricula, :nombre, :apellido)");
+
+        $query->bindValue(":matricula", $data["matricula"]);
+        $query->bindValue(":nombre", $data["nombre"]);
+        $query->bindValue(":apellido", $data["apellido"]);
+
+        $query->execute();
+
+        header("location: discentes");
+    }
+
+    public function show($pk) {
+        $query = $this->connection->prepare("SELECT * FROM discentes WHERE matricula = :matricula");
+        $query->execute([
+            ":matricula" => $pk
+        ]);
+
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        // var_dump($result);
+    }
+
+    public function edit() {
+    }
+
+    public function update($data, $pk) {
+        $query = $this->connection->prepare("UPDATE discentes SET
+            matricula = :matricula,
+            nombre = :nombre,
+            apellido = :apellido
+            WHERE matricula = :pk;");
+        
+        $query->bindValue(":matricula", $data["matricula"]);
+        $query->bindValue(":nombre", $data["nombre"]);
+        $query->bindValue(":apellido", $data["apellido"]);
+        $query->bindValue(":pk", $pk);
+        
+        $query->execute();
+    }
+
+    public function destroy($pk) {
+        $query = $this->connection->prepare("DELETE FROM discentes WHERE matricula = :matricula");
+        $query->execute([
+            ":matricula" => $pk
+        ]);
+    }
+
+}
