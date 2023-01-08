2ec4d7391af997df89a8eb554d2cd23d6ba450cd
chirs <yosoypro_117@hotmail.com>
2023-01-08 12:44:24 -0600

    apoyos test

---
+++ test/apoyosTest.php
@@ -0,0 +1,25 @@
+<?php
+
+require "../vendor/autoload.php";
+
+use App\Controllers\ApoyosController;
+
+$apoyos_controller = new ApoyosController;
+
+$apoyos_controller->store([
+    "matricula" => "A-10085643",
+    "nombre" => "Paola",
+    "apellido" => "Hernandez"
+]);
+
+// $apoyos_controller->index();
+
+// $apoyos_controller->show("A-10059375");
+
+// $apoyos_controller->update([
+//     "matricula" => "A-10059375",
+//     "nombre" => "Paola",
+//     "apellido" => "Martinez"
+// ], "A-10061486");
+
+// $apoyos_controller->destroy("A-10059375");
