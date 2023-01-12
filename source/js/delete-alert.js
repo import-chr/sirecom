af7ae59838faf67f81956db42bf7e7190bbd6f0e
chirs <yosoypro_117@hotmail.com>
2023-01-12 15:26:17 -0600

    prevent delete

--- source/js/delete-alert.js
+++ source/js/delete-alert.js
@@ -1,10 +1,11 @@
 const del = document.getElementsByClassName("trash");
 
 for(let i = 0; i < del.length; i++) {
-    del[i].addEventListener("mouseover", () => {
+    del[i].addEventListener("click", () => {
         let auth = confirm("Se borrará el registro");
 
         if(!auth) {
+            event.preventDefault();
             console.log("Cancelado");
         } else {
             console.log("Borrado");
