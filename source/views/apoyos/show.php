af24ada2e73b25b2b3456d8d3cca30528ccb4f04
chirs <yosoypro_117@hotmail.com>
2023-01-13 10:29:24 -0600

    show file

---
+++ source/views/apoyos/show.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../source/css/tables-style.css">
+    <link rel="stylesheet" href="../source/icons/uicons-solid-rounded/css/uicons-solid-rounded.css">
+    <title>Personal</title>
+</head>
+<body>
+    <div class="container">
+        <h1></h1>
+
+        <table class="content-table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Matrícula Discente</th>
+                    <th>Dirección MAC</th>
+                    <th>Sistema Operativo</th>
+                    <th>Procesador</th>
+                    <th>Memoria RAM</th>
+                    <th>Marca</th>
+                    <th>Modelo</th>
+                    <!-- <tr>Fecha de Registro</tr> -->
+                    <th>Matrícula Personal</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($results as $result): ?>
+                    <tr>
+                        <td><?= $result["pc_id"] ?></td>
+                        <td><?= $result["discente_matricula"] ?></td>
+                        <td><?= $result["direccion_MAC"] ?></td>
+                        <td><?= $result["sistema_operativo"] ?></td>
+                        <td><?= $result["procesador"] ?></td>
+                        <td><?= $result["memoria_RAM"] ?></td>
+                        <td><?= $result["marca"] ?></td>
+                        <td><?= $result["modelo"] ?></td>
+                        <!-- <td><?= $result["fecha_registro"] ?></td> -->
+                        <td><?= $result["apoyos_matricula"] ?></td>
+                        <td><a class="trash" href="pc/delete/<?= $result["pc_id"] ?>"><i class="fi-sr-trash"></a></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+
+        <a class="url-register" href="pc/create">Nueva Computadora</a>
+    </div>
+    <script src="../source/js/delete-alert.js"></script>
+</body>
+</html>
