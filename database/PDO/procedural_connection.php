e6c58858aa55e0224947c209ca2276b307568df0
chirs <yosoypro_117@hotmail.com>
2023-01-30 12:29:32 -0600

    conexion win-deb host

--- database/PDO/procedural_connection.php
+++ database/PDO/procedural_connection.php
@@ -1,7 +1,7 @@
 <?php
 
-$server = "localhost";
-$database = "SIRECOM";
+$server = "10.21.34.101";
+$database = "restaurante";
 $username = "chrlr";
 $password = "root";
 
@@ -12,3 +12,16 @@ try {
 } catch(PDOException $e) {
     echo "Conexion fallida {$e->getMessage()}";
 }
+
+/* ========================== NOTAS ========================== */
+// apuntar a la ip de maquina virtual
+
+// consola linux
+
+// - ajustar privilegios de usuario (chrlr) desde root en mysql
+// -> GRANT ALL ON *.* TO 'chrlr'@'0.0.0.0' IDENTIFIED BY 'root';
+// 0.0.0.0 hace referencia a la ip de la maquina física
+// -> FLUSH PRIVILEGES;
+// -> SELECT host FROM mysql.user WHERE user = 'chrlr';
+// comprobacion de hosts
+/* ========================== NOTAS ========================== */
